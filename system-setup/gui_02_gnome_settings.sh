0d51abba73116a81bb1394283247691b00b4937e
Daniel Carr <d.douglas.carr@gmail.com>
2018-06-02 16:17:04 +1000

    added extension settings

--- system-setup/gui_02_gnome_settings.sh
+++ system-setup/gui_02_gnome_settings.sh
@@ -8,3 +8,9 @@ dconf write /org/gnome/software/allow-updates "false"
 dconf write /org/gnome/software/download-updates "false"
 
 dconf write /org/gnome/desktop/input-sources/xkb-options "['terminate:ctrl_alt_bksp', 'caps:escape', 'nbsp:level4', 'numpad:mac', 'shift:both_capslock']"
+
+dconf write /org/gnome/shell/enabled-extensions "[ \
+  'alternate-tab@gnome-shell-extensions.gcampax.github.com', \
+  'launch-new-instance@gnome-shell-extensions.gcampax.github.com' \
+]"
+
