a34727f1883df2ee9f38781081f76489cad41d26
Daniel Carr <d.douglas.carr@gmail.com>
2020-07-30 09:42:21 +1000

    cask setup for emacs

--- system-setup/system_50_emacs.sh
+++ system-setup/system_50_emacs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+# install cask
 curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
+
+# install emacs packages
+cd /home/daniel/.emacs.d
+cask install
 
