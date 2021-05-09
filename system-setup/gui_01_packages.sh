1beadde87a921c298c84e3bec351c54d9daeeed9
Daniel Carr <d.douglas.carr@gmail.com>
2021-05-10 09:49:53 +1000

    install flatpak, use it for spotify and zotero

--- system-setup/gui_01_packages.sh
+++ system-setup/gui_01_packages.sh
@@ -3,8 +3,10 @@ set -euo pipefail
 
 export PACKAGES="\
   alacarte \
+  flatpak \
   gimp \
   gnome-shell-extensions \
+  gnome-shell-extension-autohidetopbar \
   gnome-tweak-tool \
   hedgewars \
   meld \
@@ -31,3 +33,6 @@ sudo apt-get install -y ${PACKAGES}
 echo "--- Uninstalling default packages"
 echo ${REMOVE_DEFAULT_PACKAGES}
 sudo apt-get remove -y ${REMOVE_DEFAULT_PACKAGES}
+
+echo "--- Installing flatpaks"
+flatpak install -y zotero spotify
--- system-setup/gui_30_zotero.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/install.sh | sudo bash
-sudo apt update
-sudo apt install zotero
