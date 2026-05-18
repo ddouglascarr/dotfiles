3057635f6db98aeed74e9a366e491a220c03bc42
Daniel Carr <daniel.carr@alliancesoftware.com.au>
2026-05-18 12:18:55 +1000

    paste bracket the text getting sent to lms chat

--- config/nvim/lua/scripts/lmsgpt.lua
+++ config/nvim/lua/scripts/lmsgpt.lua
@@ -6,12 +6,13 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   buffer = file_buf,
   once = true,
   callback = function()
-    local lines = vim.api.nvim_buf_get_lines(file_buf, 0, -1, false)
-    local text = table.concat(lines, "\n") .. "\n"
-    -- vim.cmd("split")
     local t = vim.fn.termopen(vim.fn.expand("~/.local/bin/lmsgpt"))
     vim.defer_fn(function()
-      vim.fn.chansend(t, text)
+      local f = io.open(filename, "r")
+      local text = f:read("*a")
+      f:close()
+      -- paste bracket the stuff
+      vim.fn.chansend(t, "\x1b[200~" .. text .. "\x1b[201~")
     end, 500)
   end
 })
